8dbf99e802094920f0a463567b5afec3c93f12ac
vruschitha <vruschithashanker@gmail.com>
2019-11-19 20:55:45 +0530

    triangle deleted

--- triangle.py
+++
@@ -1,9 +0,0 @@
-import turtle
-nofsides=20
-turtle.color("red")
-for i in range(nofsides):
-    turtle.forward(100)
-    turtle.right(360/nofsides)
-    for j in range(nofsides):
-        turtle.forward(50)
-        turtle.right(360 / nofsides)
